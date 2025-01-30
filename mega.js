52628613cff99bfa0d83b9fb7468669833077fa7
MRSHABAN40 <shabanmr086@gmail.com>
2025-01-30 17:52:04 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'shabansahibzada2@gmail.com', // Replace with your Mega email
+    email: 'sobxmd8@gmail.com', // Replace with your Mega email
     password: 'sobxmd8@', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
